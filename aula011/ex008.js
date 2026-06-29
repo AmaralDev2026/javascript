afe457ae8e66c8ee8c02ceb735d075b937d7aaac
AmaralDev2026 <amaraldev52@gmail.com>
2026-06-29 14:26:39 -0300

    aula010

---
+++ aula011/ex008.js
@@ -0,0 +1,8 @@
+var vel = 10
+console.log(`Avelocidade do seu carro é ${vel} km/h`)
+
+if (vel > 60){ // condição simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log(`dirija sempre com cinto de segurança! `)
