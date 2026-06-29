e857fc3673b7eb5e289177aad5d84e7c59d3f50c
AmaralDev2026 <amaraldev52@gmail.com>
2026-06-29 11:34:00 -0300

    reformulado

---
+++ aula06/script.js
@@ -0,0 +1,18 @@
+var p1 = window.document.getElementsByTagName('p')[0]
+p1.style.background = 'yellow'
+
+var d = window.document.getElementById('msg')
+d.style.background = 'green'
+
+
+/*seleção de elementos:
+Marca  => getElementsByTagName()[]
+Por ID => getElementById()
+Nome   => getElementsByName()[]
+Classe => getElementsByClassName()[]
+Detalhe quando o "Element" estiver com final "s" necessário colocar "[]" para selecionar qual?
+
+querySelector()
+querySelectorAll()
+
+*/
