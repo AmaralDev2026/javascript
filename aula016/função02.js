93cc2381a7f510143553a1b28209f214f637a481
AmaralDev2026 <amaraldev52@gmail.com>
2026-07-13 12:49:08 -0300

    funções aula016

---
+++ aula016/função02.js
@@ -0,0 +1,4 @@
+function soma(n1=0, n2=0) {
+    return n1 + n2
+}
+console.log(soma(7))//isso porque informei o parametro "=0" então ele soma o numero mais 0, se não definir parametro =0 vai dar erro "NaN"
